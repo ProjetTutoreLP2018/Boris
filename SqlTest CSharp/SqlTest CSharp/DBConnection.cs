ce81826ada62a293043060e4e5f0a89bfa61a0bb
Brs <brsjdtendo@gmail.com>
2018-03-28 10:01:06 +0200

    Ajout code accès BDD

---
+++ SqlTest CSharp/SqlTest CSharp/DBConnection.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace Data
+{
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                //Paramètres de connection : Serveur=ip serveur, UID = login et password = mdp du phpmyadmin
+                string connstring = string.Format("Server=localhost; database={0}; UID=root; password=root", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ SqlTest CSharp/SqlTest CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Data;
+using MySql.Data.MySqlClient;
+using System;
+
+
+namespace SqlTest_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbCon = DBConnection.Instance();
+            //Nom de la BDD
+            dbCon.DatabaseName = "jdr";
+            if (dbCon.IsConnect())
+            {
+                //Exemple de requète
+                string query = "SELECT * FROM perso";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string id = reader.GetString(0);
+                    string name = reader.GetString(1);
+                    string niv = reader.GetString(8);
+                    if (niv.Equals("-1")) niv = "?"; 
+                    string elem = "";
+                    if (reader.GetString(7).Equals("") && reader.GetString(6).Equals(""))
+                    {
+                        elem = "(Pas d'élément)"; 
+                    } else if (!reader.GetString(6).Equals("") && reader.GetString(7).Equals("")) {
+                        elem = "(" + reader.GetString(6) + ")";
+                    } else {
+                        elem = "(" + reader.GetString(6) + " / " + reader.GetString(7) + ")";
+                    }
+                    Console.WriteLine(id + "," + name + " " + elem +" : niv "+ niv );
+                }
+                Console.ReadLine();
+                dbCon.Close();
+                
+            }
+            /*string langue = Console.ReadLine();
+                string query = "INSERT INTO langue (Nom) VALUES ('" + langue + "')";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                cmd.ExecuteReader();
+
+                dbCon.Close();*/
+        }
+    }
+}
